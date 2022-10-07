bb699a1a1725d4328a332e08506da3630bd70072
Peter Wang <msnwangping@hotmail.com>
2022-10-07 14:57:59 +0100

    update version

--- scripts/prepare_release.bat
+++ scripts/prepare_release.bat
@@ -1,2 +1,9 @@
+echo off
+IF "%1"=="" GOTO:arg_not_exists
+IF "%2"=="" GOTO:arg_not_exists
+set version=%1
+set versionName=%2
 node update_API_version.js
-node update_Plugin_version.js changeme change.me.please
+node update_Plugin_version.js %version% %versionName% 
+:arg_not_exists
+echo Please run this command with version and versionName. For example: prepare_release.bat 46 46.0.1
