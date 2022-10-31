4c4ab06379068eacf32a45c2ae65b66c7d0cf4f3
Peter Wang <msnwangping@hotmail.com>
2022-10-31 15:51:58 +0000

    change userAgent's voicesdk version, it was hardcoded into 0.0.1, now it will be auto updated by the release scripts

--- Source/Wit/Private/Wit/Request/HTTP/WitHttpRequest.cpp
+++ Source/Wit/Private/Wit/Request/HTTP/WitHttpRequest.cpp
@@ -11,7 +11,7 @@
 #include "GenericPlatform/GenericPlatformHttp.h"
 #include "Wit/Utilities/WitLog.h"
 
-const FString FWitHttpRequest::WitSdkVersion = FString("0.0.1");
+const FString FWitHttpRequest::WitSdkVersion = FString("46.0.2");
 
 /**
  * Destructor
--- scripts/update_Plugin_version.js
+++ scripts/update_Plugin_version.js
@@ -12,12 +12,18 @@ const VERSION_KEY_STRING_END = ',';
 const VERSION_NAME_KEY_STRING_START = '"VersionName": "';
 const VERSION_NAME_KEY_STRING_END = '"';
 
+
+const WIT_HTTP_REQUEST_VERSION_STRING_START = 'const FString FWitHttpRequest::WitSdkVersion = FString("';
+const WIT_HTTP_REQUEST_VERSION_STRING_END = '");';
+
 console.log(process.argv);
 
 if(process.argv.length < 4) {
       console.log('Please call this script with : node update_Plugin_version.js 123 1.2.3');
 }
 
-updateFileAsync('..\\Wit.uplugin', VERSION_KEY_STRING_START,process.argv[2] , VERSION_KEY_STRING_END, ()=>{
-  updateFileAsync('..\\Wit.uplugin', VERSION_NAME_KEY_STRING_START,process.argv[3] , VERSION_NAME_KEY_STRING_END);
+updateFileAsync('..\\Wit.uplugin', VERSION_KEY_STRING_START,process.argv[2], VERSION_KEY_STRING_END, ()=>{
+  updateFileAsync('..\\Wit.uplugin', VERSION_NAME_KEY_STRING_START,process.argv[3], VERSION_NAME_KEY_STRING_END);
 });
+
+updateFileAsync('..\\Source\\Wit\\Private\\Wit\\Request\\HTTP\\WitHttpRequest.cpp', WIT_HTTP_REQUEST_VERSION_STRING_START, process.argv[3], WIT_HTTP_REQUEST_VERSION_STRING_END);
