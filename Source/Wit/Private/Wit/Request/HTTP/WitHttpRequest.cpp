d01d6065cb8e3540042fdc9d562c4eb651739873
Peter Wang <msnwangping@hotmail.com>
2023-01-26 12:29:03 +0000

    49.0.1

--- Source/Wit/Private/Wit/Request/HTTP/WitHttpRequest.cpp
+++ Source/Wit/Private/Wit/Request/HTTP/WitHttpRequest.cpp
@@ -11,7 +11,7 @@
 #include "GenericPlatform/GenericPlatformHttp.h"
 #include "Wit/Utilities/WitLog.h"
 
-const FString FWitHttpRequest::WitSdkVersion = FString("47.0.4");
+const FString FWitHttpRequest::WitSdkVersion = FString("49.0.1");
 
 /**
  * Destructor
